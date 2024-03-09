f5d22d166a14b56aff2f88275ac93bc88e01a563
Siddhesh Jagtap <114938387+siddhesh2017@users.noreply.github.com>
2024-03-09 21:00:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,60 @@
-let imgURL;
-function handleUpload()
-{
-    const inputFile = document.getElementById('ipFile');
-    const img = inputFile.files[0];
+  /*here math.random() gives a random number between 0-1 excluding 0&1 (ex: 0.4392532) so we 
+  multiply it by 10 to get one digit number, then the number will be still in point (ex:2.54538403)
+  to remove the part after point we use Math.floor() to get single digit 2.
+  */ 
+ /*
+ Math.random() => to get random no 0-1
+ *10 => to get single digit deci no
+ Math.floor() => to get single digit no
+ */ 
+/*
+    here we used clearInterval() because the count will continue in negative taking up the space to stop interval we use clearInterval()
+*/
 
-    const imgData = new FormData();
-    imgData.append("image_file",img);
-    imgData.append("size", 'auto');
+ let tmr_cnt = 60;
+ let score = 0;
+ let hitno = 0;
 
-    const apiKey = '79P5tbn3DUU1MaFthmDJytrb';
-    fetch(
-        'https://api.remove.bg/v1.0/removebg',
-        {
-            method:'POST',
-            headers:{
-                'X-Api-Key': apiKey
-            },
-            body:imgData,
+function makeBubbles() {  
+    let clutter = '';
+    for(let i = 1; i<=168; i++){
+        let num = Math.floor(Math.random()*10);
+        clutter += `<div class="bubble">${num}</div>`;
+    }
+    document.querySelector("#pbottom").innerHTML = clutter;
+}
+function runTimer() {
+    var tmr_int = setInterval(function(){
+        if(tmr_cnt > 0){
+            tmr_cnt--;
+            document.querySelector('#time').textContent = tmr_cnt;
         }
-    )
-    .then(function(response){
-        return response.blob();
-    })
-    .then(function(blob){
-        console.log(blob);
-
-        const imgUrl = URL.createObjectURL(blob);
-        const blobImg = document.createElement('img');
-        blobImg.src = imgUrl;
-        blobImg.style.background = 'rgb(1,1,1,0.5)';
-        blobImg.style.borderBottomLeftRadius = "20px";
-        blobImg.style.borderBottomRightRadius = "20px";
-        blobImg.style.borderTopLeftRadius = "20px";
-        blobImg.style.borderTopRightRadius = "20px";
-        blobImg.style.width = 'auto';
-        blobImg.style.height = 'auto';
-
-        imgURL = imgUrl
-        document.body.appendChild(blobImg);
-        const secondCol = document.querySelector('.col-md-6:nth-child(2)');
-        secondCol.appendChild(blobImg);
-    })
-    .catch();
+        else{
+            clearInterval(tmr_int);
+            document.querySelector('#pbottom').innerHTML = "<h1 id='gameover'>GameOver</h1>";
+        }
+    }, 1000)
 }
 
-function downloadFile(){
-    var imgDownload = document.createElement('a');
-    imgDownload.href = imgURL;
-    imgDownload.download = 'sid-bg.png';
-    document.body.appendChild(imgDownload);
-    imgDownload.click();
-    document.body.removeChild(a)
+function randHit(){
+    hitno = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = hitno;
 }
+
+function incScore(){
+    score += 10;
+    document.querySelector("#score").textContent = score;
+}
+
+document.querySelector('#pbottom').addEventListener('click',function(event){
+    let clickednum = Number(event.target.textContent);
+    if(clickednum === hitno){
+        incScore();
+        makeBubbles();
+        randHit();
+    }
+})
+
+runTimer();
+makeBubbles();
+randHit();
